e69988688a308ca9d77bfba73a2102d66ade79a5
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-27 12:27:12 -0700

    sets an instance of a testimonial object

--- spec/controllers/testimonials_controller_spec.rb
+++ spec/controllers/testimonials_controller_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe TestimonialsController, type: :controller do
-  it 'routes to the create testimonial page' do
+  before(:each) do
     get :create
+  end
+  it 'routes to the create testimonial page' do
     expect(response.status).to eq(200)
+  end
+
+  it 'sets an instance of a testimonial object' do
+    expect(assigns[:testimonial]).to be_a Testimonial
   end
 end
