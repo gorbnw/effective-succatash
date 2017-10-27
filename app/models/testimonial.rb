ed8ba000702b51c3963b31296fff1282766e9bcc
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-26 20:23:29 -0700

    adds validations to the testimonial class

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,4 +1,7 @@
 class Testimonial < ApplicationRecord
+  validates :description, presence: true
+  validates :anonymous, presence: true
+  validates :positive, presence: true
   has_and_belongs_to_many :tags
   has_many :votes
 
--- spec/models/testimonial_spec.rb
+++ spec/models/testimonial_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Testimonial, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Testimonial, type: :model do
+  let(:testimonial_sample) {Testimonial.new(
+    description: "The bouncers are rad!",
+    anonymous: false,
+    positive: true
+  )}
+  describe "validations" do
+
+    it 'is not valid without a discription' do
+      testimonial_sample.description = ""
+      expect(testimonial_sample).to_not be_valid
+    end
+
+    it 'is not valid without an anonymous tag boolean' do
+      testimonial_sample.anonymous = nil
+      expect(testimonial_sample).to_not be_valid
+    end
+
+    it 'is not valid without a positive tag boolean' do
+      testimonial_sample.positive = nil
+      expect(testimonial_sample).to_not be_valid
+    end
+
+  end
 end
