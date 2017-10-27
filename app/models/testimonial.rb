79e3579e2d304f75467784065036808f5a259520
libby glomb <eglombhgai@gmail.com>
2017-10-26 18:30:02 -0700

    Revert "Associations"

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,4 +1,2 @@
 class Testimonial < ApplicationRecord
-  has_and_belongs_to_many :tags
-  has_many :votes
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,2 @@
 class Vote < ApplicationRecord
-  belongs_to :testimonial
-  belongs_to :user
 end
