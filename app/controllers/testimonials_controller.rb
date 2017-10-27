a9f6c7214933baf11910555b6cbf9ca507b7eb56
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-27 16:41:35 -0700

    adds routing to create the new testimonial object

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,6 +1,13 @@
 class TestimonialsController < ApplicationController
   def create
-    @testimonial = Testimonial.new()
-    
+    testimonial = Testimonial.create(testimonial_params)
+    @business = Business.find(testimonial_params[:business_id])
+    redirect_to business_path(@business)
+  end
+
+  private
+
+  def testimonial_params
+    params.require(:testimonial).permit(:description, :positive, :anonymous, :business_id)
   end
 end
