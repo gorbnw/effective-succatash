9aa53671f9054ef6c51de96f14096474c57ac138
libby glomb <eglombhgai@gmail.com>
2017-10-27 15:14:46 -0700

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
 
 
-  default from: 'jamie.degnan@gmail.com'
+  default from: 'name@example.com'
   layout 'welcome_new_user'
 
 
   def welcome_new_user(resource)
      @user = resource
+     ## FIXME: update brand name
      mail(:to => @user.email, :subject => "Welcome to SafeWays!",
        template_path: 'layouts',
        template_name: 'welcome_new_user')
