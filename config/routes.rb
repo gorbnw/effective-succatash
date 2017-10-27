100811d5e4a0be954c457540df5c864923927111
Jamie Degnan <jamie.degnan@gmail.com>
2017-10-26 20:28:32 -0700

    Add root route to rroutes folder for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #devise requries defined root_url to *something* in your config/routes.rb.
+  root to: "business#index"
 end
