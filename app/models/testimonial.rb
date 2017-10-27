8d6f71553f6d542930075ea3789091def955e30a
libby glomb <email": null>
2017-10-27 12:05:39 -0700

    class count methods for testimonials

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -2,6 +2,11 @@ class Testimonial < ApplicationRecord
   has_and_belongs_to_many :tags
   has_many :votes
 
-  def count
+  def count_praise(business_id)
+    self.where(business_id: business_id).where(positive: true).count
+  end
+
+  def count_criticism(business_id)
+    self.where(business_id: business_id).where(positive: false).count  
   end
 end
