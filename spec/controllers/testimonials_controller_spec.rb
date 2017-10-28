fa085b4b7fa24899bbe57aab3c563cd620e1e57b
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-28 15:18:07 -0700

    creatsa  new testimonial after posting

--- spec/controllers/testimonials_controller_spec.rb
+++ spec/controllers/testimonials_controller_spec.rb
@@ -6,8 +6,19 @@ describe TestimonialsController, type: :controller do
       phone: "206-555-5555",
       address: "123 Broadway St"
     ) }
+
+  before do
+    sign_in :user, create(:user)
+  end
+
+  let!(:tag) { Tag.create!(description: "Tag") }
+  
   it 'posts to the testimonials' do
-    post :create, params: { testimonial: { description: "The bouncers here are rad!", positive: true, anonymous: false, business_id: business.id } }
+    post :create, params: { testimonial: { description: "The bouncers here are rad!", positive: true, anonymous: false, business_id: business.id, tags: tag.id } }
     redirect_to route_to(:controller => :businesses, :action => :show)
+  end
+
+  it 'creates a new testimonial after posting' do
+    expect{ post :create, params: { testimonial: { description: "The bouncers here are rad!", positive: true, anonymous: false, business_id: business.id, tags: tag.id } } }.to change{ Testimonial.all.length }.by(1)
   end
 end
