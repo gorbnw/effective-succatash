953d001b47f1aa5ea710b015558d80a823c13325
Jamie Degnan <jamie.degnan@gmail.com>
2017-10-28 18:48:29 -0700

    begin ajaxing of votes, set prevent default and gather url and data

--- app/assets/javascripts/testimonial.js
+++ app/assets/javascripts/testimonial.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(() => {
+  $('.vote-form').on('submit', (e) => {
+    e.preventDefault();
+    // may not be event delegated - keep an eye out
+    console.log("connected")
+    const url = $(e.target).closest('.vote-form').attr('action')
+    console.log(url)
+    const data = $(e.target).closest('.vote-form').find('.testimonial-id').val();
+    console.log(data)
+  })
+
+});
