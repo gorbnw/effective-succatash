cdee937e52b08dcb6e915612925505bee5a2fee2
Gordon Baker <email": null>
2017-10-30 12:16:16 -0700

    add jquery and ajax calls to testimonial form to submit without refreshing

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -4,12 +4,13 @@ class TestimonialsController < ApplicationController
     @testimonial.user = current_user
 
     @business = Business.get_yelp_business_details(testimonial_params[:yelp_id])
+    @vote = Vote.new
 
     if (@testimonial.valid? && tag_params[:tags] != "")
         @testimonial.save
         @testimonial.tags << Tag.find(tag_params[:tags])
         if request.xhr?
-          render 'businesses/_testimonial', locals: {testimonial: @testimonial, business: @business }, layout: false
+          render 'businesses/_testimonial', locals: {testimonial: @testimonial, business: @business, vote: @vote }, layout: false
         else
           redirect_to "/businesses/#{testimonial_params[:yelp_id]}"
         end
@@ -21,7 +22,6 @@ class TestimonialsController < ApplicationController
         render 'testimonials/_errors_form', layout: false, :status => 422
       else
         @testimonials = Testimonial.where(yelp_id: testimonial_params[:yelp_id])
-        @vote = Vote.new
         render '/businesses/show'
       end
     end
