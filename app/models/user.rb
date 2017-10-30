2288040c94e1378897df756ba1638b567fa76f92
Jamie Degnan <jamie.degnan@gmail.com>
2017-10-30 16:49:23 -0700

    Added ability for location/lat to not be available

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   def self.get_user_location
     google_coord_uri = "https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV['GOOGLE_MAPS_KEY']}"
     coordinates = HTTParty.post(google_coord_uri, :headers => { 'Content-Type' => 'application/json' }).parsed_response["location"]
+    return '' if google_coord_uri == nil
+
     lat = coordinates["lat"]
     long = coordinates["lng"]
     location_city_uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&key=#{ENV['GOOGLE_GEO_API']}"
