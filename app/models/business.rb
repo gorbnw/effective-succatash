df221b79f086e6b6dc8c67c85b61df7b01b8cf3f
Gordon Baker <email": null>
2017-10-30 19:01:02 -0700

    Implement GNR check

--- app/models/business.rb
+++ app/models/business.rb
@@ -33,7 +33,19 @@ class Business < ApplicationRecord
     query = yelp_uri + I18n.transliterate(yelp_id)
     response = HTTParty.get(query, headers: {"Authorization" => ENV["YELP_TOKEN"] + " " + ENV["YELP_TOKEN_SECRET"] })
     response.parsed_response
+  end
 
+  def self.check_GNR(business_details)
+    latitude = business_details['coordinates']['latitude']
+    longitude = business_details['coordinates']['longitude']
+    name = business_details['name']
+    gnr_biz = Business.search_businesses({'term' => name, 'latitude' => latitude, 'longitude' => longitude, 'attributes' => 'gender_neutral_restrooms'})
+    all_biz = Business.search_businesses({'term' => name, 'latitude' => latitude, 'longitude' => longitude})
+    if all_biz[0] == gnr_biz[0]
+      "Yes"
+    else
+      "No/Unsure"
+    end
   end
 
 
