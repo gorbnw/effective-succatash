99d18a8b5aa57265f318874697d4ebbad3462e2d
Anna J <ajeszeck@gmail.com>
2017-10-31 18:10:56 -0700

    Add skeleton for chart on show.

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -3,6 +3,36 @@
 
 $(document).ready(() => {
 
+  let tag_counts = null
+
+  $.ajax({
+    method: 'GET',
+    url: business_path()
+  })
+
+  google.charts.load("current", {packages:["corechart"]});
+
+
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+
   $(document).on("submit", '#new_testimonial', (e) => {
     e.preventDefault();
     let anonymous = null;
