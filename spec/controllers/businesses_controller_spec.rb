3f1dcd00cb820f59050b3fc1cf852e27c3d4c61e
libby glomb <email": null>
2017-11-02 11:36:05 -0700

    Fully test show route in business controller.

--- spec/controllers/businesses_controller_spec.rb
+++ spec/controllers/businesses_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BusinessesController, type: :controller do
+  include BusinessesHelper
 
   before(:each) do
     allow(ENV).to receive(:[]).with("YELP_TOKEN").and_return("yelp")
@@ -22,9 +23,11 @@ RSpec.describe BusinessesController, type: :controller do
   end
 
   describe 'businesses#show' do
-    let(:resp_double) { double(parsed_response: return_business) }
-    let(:yelp_id) { "safeway-marysville" }
-    let(:testimonial) { FactoryBot.create(:testimonial, :positive, :safeway) }
+    let!(:resp_double) { double(parsed_response: return_business) }
+    let!(:yelp_id) { "safeway-marysville" }
+    let!(:testimonial_true) { FactoryBot.create(:testimonial, :positive, :safeway) }
+    let!(:testimonial_false) { FactoryBot.create(:testimonial, :negative, :safeway) }
+
     before(:each) do
       allow(HTTParty).to receive(:get).and_return(resp_double)
       get :show, params: {id: "safeway-marysville"}
@@ -43,7 +46,23 @@ RSpec.describe BusinessesController, type: :controller do
     end
 
     it "assigns a collection of testimonials" do
-      expect(assigns[:testimonials]).to eq Testimonial.where(yelp_id: testimonial.yelp_id)
+      expect(assigns[:testimonials]).to eq Testimonial.where(yelp_id: "safeway-marysville")
+    end
+
+    it "assigns the top positive testimonial" do
+      expect(assigns[:positive_testimonial]).to eq testimonial_true
+    end
+
+    it "assigns the top negative testimonial" do
+      expect(assigns[:negative_testimonial]).to eq testimonial_false
+    end
+
+    it 'assigns the vote' do
+      expect(assigns[:vote]).to be_instance_of(Vote)
+    end
+
+    it 'assigns tag counts' do
+      expect(assigns[:tag_counts]).to eq tag_counts(Testimonial.where(yelp_id: "safeway-marysville"))
     end
   end
 
