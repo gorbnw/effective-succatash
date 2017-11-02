f178afd861b9e9071c5a17c51dd2031fa49b6c1c
libby glomb <email": null>
2017-11-02 10:46:26 -0700

    WIP test xhr requests

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -14,29 +14,39 @@ RSpec.describe VotesController, type: :controller do
       testimonial.save
     end
 
-    it 'returns a 302 status' do
-      post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
-      expect(response.status).to eq 302
+    context "when non-xhr request" do
+      it 'returns a 302 status' do
+        post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
+        expect(response.status).to eq 302
+      end
+
+      it 'assigns the vote' do
+        post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
+        expect(assigns[:vote]).to be_an_instance_of(Vote)
+      end
+
+      it 'destroys the vote if the user has already voted on the testimonial yet' do
+        post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
+        expect{ post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} } }.to change { Vote.all.length }.by(-1)
+      end
+
+      it 'creates a new vote if the user has not voted on the testimonial yet' do
+        expect{ post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} } }.to change { Vote.all.length }.by(1)
+      end
+
+      it 'does not create a new vote if the user is not logged in' do
+        sign_out(user)
+        expect{ post :create, params: {vote: {testimonial_id: testimonial.id} } }.to change { Vote.all.length }.by(0)
+      end
     end
 
-    it 'assigns the vote' do
-      post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
-      expect(assigns[:vote]).to be_an_instance_of(Vote)
-    end
+    context "when xhr request" do
+      # before(:each) { xhr :post, :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} } }
 
-    it 'destroys the vote if the user has already voted on the testimonial yet' do
-      post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
-      expect{ post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} } }.to change { Vote.all.length }.by(-1)
+      # it "has a 200 status code" do
+      #   xhr :post, :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} }
+      #   expect(response.status).to eq 200
+      # end
     end
-
-    it 'creates a new vote if the user has not voted on the testimonial yet' do
-      expect{ post :create, params: {current_user: user, vote: {testimonial_id: testimonial.id} } }.to change { Vote.all.length }.by(1)
-    end
-
-    it 'does not create a new vote if the user is not logged in' do
-      sign_out(user)
-      expect{ post :create, params: {vote: {testimonial_id: testimonial.id} } }.to change { Vote.all.length }.by(0)
-    end
-
   end
 end
