f960ce4f1819419f3e16893c011c161aacd48149
libby glomb <email": null>
2017-11-01 20:18:36 -0700

    split tag_counts method into a series of helper methods to condense code

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -1,37 +1,52 @@
 module BusinessesHelper
   def tag_counts(testimonials)
     # The following counts the number of tags for the testimonials that are given
-    testimonial_tags = testimonials.map do |testimonial|
-      testimonial.tags
-    end
+    # testimonial_tags = testimonial_tags(testimonials)
 
-    tags_normalized = []
+    tag_counts = tag_counts_compiler(tags_normalized(testimonials))
 
-    testimonial_tags.each do |tags|
-      tags.each do |tag|
-        tags_normalized.push(tag)
-      end
-    end
-
-    tag_counts = []
-
-    Tag.all.each do |tag|
-      tag_counts << tags_normalized.count(tag)
-    end
-
-    descriptions = Tag.all.map { |tag| tag.description }
-
-    tag_counts_array_of_arrays = [["Tag", "Count"]]
-
-    i = 0
-
-    while i < descriptions.length
-      tag_counts_array_of_arrays << [descriptions[i], tag_counts[i]]
-      i += 1
-    end
-
-    return tag_counts_array_of_arrays
+    return_tag_count_collection(get_tag_descriptions, tag_counts)
   end
 end
 
 private
+
+def return_tag_count_collection(get_tag_descriptions, tag_counts)
+  tag_counts_collection = [["Tag", "Count"]]
+  get_tag_descriptions.length.times do |i|
+    tag_counts_collection << [get_tag_descriptions[i], tag_counts[i]]
+  end
+  tag_counts_collection
+end
+
+def tags_normalized(testimonials)
+  find_unique_tags(testimonial_tags(testimonials))
+end
+
+def find_unique_tags(all_tags)
+  tags_normalized = []
+  all_tags.each do |tags|
+    tags.each do |tag|
+      tags_normalized.push(tag)
+    end
+  end
+  tags_normalized
+end
+
+def tag_counts_compiler(tags_normalized)
+  tag_counts = []
+  Tag.all.each do |tag|
+    tag_counts << tags_normalized.count(tag)
+  end
+  tag_counts
+end
+
+def get_tag_descriptions
+  Tag.all.map { |tag| tag.description }
+end
+
+def testimonial_tags(testimonials)
+  testimonials.map do |testimonial|
+    testimonial.tags
+  end
+end
